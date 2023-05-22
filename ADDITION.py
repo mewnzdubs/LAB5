8e929c07a966015631c6b5f778808e676b7f1857
mewnzdubs <134245242+mewnzdubs@users.noreply.github.com>
2023-05-21 19:51:26 -0700

    Update ADDITION.py

--- ADDITION.py
+++ ADDITION.py
@@ -1,7 +1,4 @@
 def add_three():
-    """
-    This function takes user input, adds 3 to it, and returns the result.
-    """
     number = float(input("Enter a number: "))
     result = number + 3
     return result
