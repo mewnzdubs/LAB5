918908a5ec9b842680991e1634e3cbc4f3daf5d5
mewnzdubs <134245242+mewnzdubs@users.noreply.github.com>
2023-05-21 19:53:08 -0700

    Update ADDITION.py

--- ADDITION.py
+++ ADDITION.py
@@ -2,3 +2,6 @@ def add_three():
     number = float(input("Enter a number: "))
     result = number + 3
     return result
+multiplied_result = result * 3
+    print("Result multiplied by 3:", multiplied_result)
+    return result
