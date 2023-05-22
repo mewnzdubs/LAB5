5740338d615174c6b759a5d834cc47163d65b369
mewnzdubs <134245242+mewnzdubs@users.noreply.github.com>
2023-05-21 19:55:09 -0700

    Update ADDITION.py
    fixed with added function

--- ADDITION.py
+++ ADDITION.py
@@ -1,7 +1,9 @@
-def add_three():
+def add_and_multiply():
     number = float(input("Enter a number: "))
     result = number + 3
+    multiplied_result = result * 3
+    print("Initial Result:", result)
+    print("Multiplied Result:", multiplied_result)
     return result
-multiplied_result = result * 3
-    print("Result multiplied by 3:", multiplied_result)
-    return result
+
+add_and_multiply()
